02156285c14bae766fb375cf165bd76002681a2e
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-04-10 19:25:05 +0200

    Update ScrpitUtili_BourneShell.sh

--- ScriptUtili_BourneShell.sh
+++ ScriptUtili_BourneShell.sh
@@ -85,13 +85,36 @@ esac
 
 # -------------------------------------------------------------------------------------------------------------- #
 
+### Verifico che il PATH sia ASSOLUTO ###
+### Verifica combinata: DIRECTORY ESISTENTE && DIRECTORY TRAVERSABILE ###
+
+# Uso un case per verificare se il parametro e' un path assoluto
+case $i in
+/*)
+    echo "Il parametro $i e' un path assoluto."
+    # Ora verifico l'esistenza della directory
+    if test -d $i -a -x $i
+    then
+        echo "Il parametro $i e' una directory traversabile."
+    else
+        echo "Il parametro $i non e' una directory traversabile. Processo interrotto."
+        # Esco specificando un valore intero di errore
+        exit 3
+    fi;;
+*)
+    echo "Il parametro $i non e' un path assoluto. Processo interrotto."
+    # Esco specificando un valore intero di errore
+    exit 2;;
+esac
+
+# -------------------------------------------------------------------------------------------------------------- #
+
 ### Ciclo con CONTATORE ###
 
 # Dichiaro una variabile da usare come contatore
 J=1
 # Scorro la lista in un ciclo che utilizza il contatore J
-for i
-do
+for i; do
     # Incremento la variabile contatore di 1 a ogni iterazione
     J=`expr $J + 1`
 done
@@ -105,8 +128,7 @@ J=1
 # Creo una variabile che utilizzero' come contenitore per la mia lista
 var=
 # Uso un ciclo per riempire la lista
-for i
-do
+for i; do
     # Inserisco il nuovo elemento in coda alla lista
     var="$var $i"
 done
@@ -135,5 +157,37 @@ case $i in
         echo "Il numero $i e' positivo."
     fi;;
 esac
+
+# -------------------------------------------------------------------------------------------------------------- #
+
+### Richiesta di INPUT BOOLEANO ###
+
+# Chiedo all'utente se vuole visualizzare in ordine alfabetico i file
+echo "Vuoi visualizzare in ordine alfabetico i file? [S/N]"
+# Leggo la risposta dell'utente
+read risposta
+# Controllo la risposta dell'utente
+case $risposta in
+S*|s*|Y*|y*)
+    # Ordino alfabeticamente i file presenti nel file temporaneo
+    sort /tmp/temp$$;;
+N*|n*)
+    # Non faccio nulla
+    ;;
+*)
+    # Risposta non valida
+    echo "Risposta non valida.";;
+esac
+
+# -------------------------------------------------------------------------------------------------------------- #
+
+### Creazione FILE TEMPORANEO CON PID ###
+### Eliminazione FILE TEMPORANEO ###
+
+# Creo un file temporaneo
+> /tmp/temp$$
+
+# Elimino il file temporaneo
+rm /tmp/temp$$
 
 # -------------------------------------------------------------------------------------------------------------- #
