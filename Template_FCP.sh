f4e7d6b20f24289d330b5f1ade8beb1240f50e0e
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-04-12 12:29:20 +0200

    Composed templates

---
+++ Template_FCP.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# Controllo che il numero di parametri rientri nella soglia desiderata
+if test $# -lt 4
+then
+    # In tal caso il numero dei parametri non riesntra nell'intervallo desiderato, quindi interrompo l'esecuzione
+    echo "Numero parametri errato. Processo interrotto."
+    # Esco specificando un valore intero di errore
+    exit 1;
+fi
+
+
+
+# Dichiaro una variabile lista per i path assoluti
+dirList=
+
+# Controllo che i parametri rimanenti siano path assoluti e al contempo directory traversabili
+for i; do
+    # Uso un case per verificare se il parametro e' un path assoluto
+    case $i in
+    /*)
+        echo "Il parametro $i e' un path assoluto."
+        # Ora verifico l'esistenza della directory e che essa sia traversabile
+        if test -d "$i" -a -x "$i"
+        then
+            echo "Il parametro $i e' una directory traversabile."
+            # Aggiungo il path alla lista
+            dirList="$dirList $i"
+        else
+            echo "Il parametro $i non e' una directory traversabile. Processo interrotto."
+            # Esco specificando un valore intero di errore
+            exit 3
+        fi;;
+    *)
+        echo "Il parametro $i non e' un path assoluto. Processo interrotto."
+        # Esco specificando un valore intero di errore
+        exit 2;;
+    esac
+done
+
+# Aggiungo alla variabile d'ambiente PATH il path corrente
+PATH=`pwd`:$PATH
+# Aggiungo le modifiche effettuate alla variabile d'ambiente esportandola
+export PATH
+
+# Creo un file temporaneo
+echo "Creo un file temporaneo."
+> /tmp/nomiAssoluti
+
+# Scorro la lista di directory
+for G in $dirList; do
+    # Chiamo lo script ricorsivo
+    FCR.sh "$G" "$param" "/tmp/nomiAssoluti"
+done
+
+
+
+# Elimino il file temporaneo
+echo "Elimino il file temporaneo."
+rm /tmp/nomiAssoluti
---
+++ Template_FCR.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Mi sposto nella directory passata come primo parametro
+cd $1
+
+# Controllo che la directory contenga almeno un file con terminazione .$2
+for F in *; do
+    
+done
