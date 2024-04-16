a49229ac28087f1342b7a6af0398bf1254f005e6
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-04-16 10:59:25 +0200

    Updaate (bruh)

--- SimulazioniEsame/Esame4/FCR.sh
+++ SimulazioniEsame/Esame4/FCR.sh
@@ -10,12 +10,13 @@ for i in *; do
         if test `echo $i | wc -c` -eq 4; then
             echo "Il file $i ha un nome di 3 caratteri."
             # Verifico che tutti i caratteri dispari (primo e terzo) siano uguali al secondo parametro
-            if test `echo $i | head -c1` = "$2" -a `echo $i | head -c3 | tail -c1` = "$2"; then
+            case $i in
+            */$2?$2)
                 # La directory soddisfa tutte le condizioni, quindi la aggiungo al file temporaneo passato come terzo parametro
-                echo "`pwd`"/"$i" >> "$3"
-            else 
-                echo "Non tutti i caratteri dispari del file $i sono uguali a $2."
-            fi
+                echo "`pwd`"/"$i" >> "$3";;
+            *)
+                echo "Non tutti i caratteri dispari del file $i sono uguali a $2.";;
+            esac
         else
             echo "Il file $i non ha un nome di 3 caratteri."
         fi
