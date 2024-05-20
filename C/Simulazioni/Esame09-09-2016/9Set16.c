3e0437776b2513eb826a0a1a6eba8dcc12c2d131
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-05-20 12:38:45 +0200

    update

--- C/Simulazioni/Esame09-09-2016/9Set16.c
+++ C/Simulazioni/Esame09-09-2016/9Set16.c
@@ -17,6 +17,7 @@ int main(int argc, char** argv)
     int N;					/* numero di file passati sulla riga di comando */
     int i,j;				/* indici per i cicli */
     pipe_t *piped;			/* array dinamico di pipe descriptors */
+    pipe_t p;               /* pipe per la comunicazione tra 26 esimo figlio e padre */
     int status;				/* variabile di stato per la wait */
     int ritorno;			/* variabile usata dal padre per recuperare valore di ritorno di ogni figlio */
     
@@ -41,7 +42,7 @@ int main(int argc, char** argv)
         exit(2);
     }
     
-    /* Creazione delle 26 pipe */
+    /* Creazione delle 26 pipe per la comunicazione tra figli */
     for (i = 0; i < 26; i++)
     {
         /* Controllo che ogni creazione vada a buon fine */
@@ -51,6 +52,14 @@ int main(int argc, char** argv)
             printf("Errore nel piping.\n");
             exit(3);
         }
+    }
+
+    /* Creo una pipe per consentire la comunicazione tra padre e ultimo figlio verificando se l'operazione va a buon fine */
+    if ((pipe(p)) < 0)
+    {
+        /* La creazione della pipe ha fallito, stampo un messaggio d'errore ed esco specificando un valore intero d'errore */
+        printf("Errore nel piping.\n");
+        exit(3);
     }
     
     /* Generazione dei 26 figli */
@@ -74,7 +83,14 @@ int main(int argc, char** argv)
             /* Chiudo i lati di pipe non utilizzati dal figlio */
             for (j = 0; j < 26; j++)
             {
-                close(piped[j][0]);
+                if (j == 25) 
+                {
+                    
+                }
+                if (j == 0 || (i - 1) != (j - 1))
+                {
+                    close(piped[j][0]);
+                }
                 if (i != j)
                 {
                     close(piped[j][1]);
