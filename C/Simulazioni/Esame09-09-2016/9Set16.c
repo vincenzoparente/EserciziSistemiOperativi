ca81e498244978a79d4be98842e84f77c81b0093
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-05-21 11:22:42 +0200

    update

--- C/Simulazioni/Esame09-09-2016/9Set16.c
+++ C/Simulazioni/Esame09-09-2016/9Set16.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 typedef int pipe_t[2];         /* definizione del TIPO pipe_t come array di 2 interi */
 
@@ -13,6 +14,32 @@ typedef struct caratteriLetti {
     char v1;        // Carattere cercato
     long int v2;    // Numero di occorrenze
 } caratteriLetti;
+
+void scambia(caratteriLetti* a, caratteriLetti* b)
+{
+    caratteriLetti tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void bubbleSort(caratteriLetti v[], int dim)
+{
+    int i;
+    bool ordinato = false;
+    while (dim > 1 && !ordinato)
+    {
+        ordinato = true;
+        for (i = 0; i < dim - 1; i++)
+        {
+            if (v[i].v2 > v[i + 1].v2)
+            {
+                scambia(&v[i], &v[i+1]);
+                ordinato = false;
+            }
+        }
+        dim--;
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -27,6 +54,12 @@ int main(int argc, char** argv)
     int ritorno;			/* variabile usata dal padre per recuperare valore di ritorno di ogni figlio */
     
     /* -------------------------------------------- */
+
+    if (argc != 2)
+    {
+        printf("Numero di parametri errato: argc = %d, ma dovrebbe essere = 2\n", argc);
+        exit(1);
+    }
     
     /* Creo una pipe per consentire la comunicazione tra figli verificando se l'operazione va a buon fine */
     for (i = 0; i < 26; i++)
@@ -57,31 +90,98 @@ int main(int argc, char** argv)
             /* Codice del figlio */
             printf("DEBUG-Esecuzione del processo figlio %d\n", getpid());
 
+            /* Chiudo i piped inutilizzati */
+            for (j = 0; j < 26; j++)
+            {
+                if (i == 0 || j != (i - 1))
+                {
+                    close(piped[j][0]);
+                }
+                if (j != i)
+                {
+                    close(piped[j][1]);
+                }
+            }
+
+            /* Leggo dalla pipe precedente i valori passati */
+            if (i != 0)
+            {
+                read(piped[i - 1][0], cl, sizeof(cl));
+            }
+
             /* Assegno alla struttura il carattere corrispondente */
             cl[i].v1 = 'a' + i;
-            /* Faccio iniziare da 0 v2 */
-            cl[i].v2 = 0;
+            /* Faccio iniziare v2 da 0 */
+            cl[i].v2 = 0L;
 
             /* Apertura del file e controllo esistenza */
-            if((fd = open(argv[2], O_RDWR)) < 0)
+            if ((fd = open(argv[1], O_RDONLY)) < 0)
             {
-                printf("Errore nell'apertura del file '%s'.\n", argv[2]);
+                printf("Errore nell'apertura del file '%s'.\n", argv[1]);
                 exit(-1);
             }
             
             /* Ciclo di lettura dei caratteri */
             char tmp;
-            while(read(fd, &tmp, 1))
+            while(read(fd, &tmp, 1) > 0)
             {
                 if (cl[i].v1 == tmp)
                 {
                     cl[i].v2++;
                 }
             }
+
+            /* Scrivo l'array di strutture sulla pipe */
+            write(piped[i][1], cl, sizeof(cl));
+
+            /* Esco scrivendo l'ultimo carattere letto */
+            exit(tmp);
         }
     }
     
     /* Codice del padre (fuori dal for perche' mi interessa eseguirlo solo quando il ciclo e' terminato) */
+
+    /* Chiudo i piped inutilizzati (lascio aperto solo l'ultimo per leggere dall'ultimo figlio) */
+    for (i = 0; i < 26; i++)
+    {
+        close(piped[i][1]);
+        if (i != 25)
+        {
+            close(piped[i][0]);
+        }
+    }
+
+    /* Leggo l'array di strutture finale */
+    read(piped[25][0], cl, sizeof(cl));
+
+    /* Ordino i risultati in ordine crescente */
+    bubbleSort(cl, 26);
+
+    /* Stampo i risultati su stdout */
+    for (i = 0; i < 26; i++)
+    {
+        printf("Ho trovato il carattere %c %ld volte nel file %s.\n", cl[i].v1, cl[i].v2, argv[1]);
+    }
+    
+    /* Aspetto i figli */
+    int pidFiglio;
+    for (i = 0; i < 26; i++)
+    {
+        if ((pidFiglio = wait(&status)) < 0)
+        {
+            printf("Errore del padre in wait.\n");
+            exit(8);
+        }
+        if ((status & 0xFF) != 0)
+        {
+            printf("Processo figlio %d terminato in modo anomalo.\n", pid);
+        }
+        else
+        {
+            ritorno = (int)((status >> 8) & 0xFF);
+            printf("Il processo figlio %d ha ritornato %d.\n", pidFiglio, ritorno);
+        }
+    }
     
     exit(0);
 }
